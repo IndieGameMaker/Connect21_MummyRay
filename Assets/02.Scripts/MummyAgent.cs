659c1ae13cea160b6990e05cec1374243c5f96d5
Lee JaeHyun <myleje@gmail.com>
2020-11-07 10:44:45 +0900

    .

--- Assets/02.Scripts/MummyAgent.cs
+++ Assets/02.Scripts/MummyAgent.cs
@@ -4,7 +4,38 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 
+//주변환경을 관측, 전달 (Observation)
+//정책에 따라서 행동 (Action)
+//행동에 따른 보상 (Reward)
 public class MummyAgent : Agent
 {
+    //에이젼트 초기화
+    public override void Initialize()
+    {
 
+    }
+
+    //학습을 시작할때마다 호출되는 메소드(스테이지-환경을 초기화)
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    //주변환경을 관측
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+
+    //정책에 따른 행동
+    public override void OnActionReceived(float[] vectorAction)
+    {
+
+    }
+
+    //개발자가 테스트용, 모방학습(Immetation Learing)
+    public override void Heuristic(float[] actionsOut)
+    {
+        
+    }
 }
