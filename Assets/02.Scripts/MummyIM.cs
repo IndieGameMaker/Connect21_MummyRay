01b68d4b4389d025f3bce83f7c21c27cbcbc157d
Lee JaeHyun <myleje@gmail.com>
2020-11-07 16:25:43 +0900

    .

---
+++ Assets/02.Scripts/MummyIM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class MummyIM : Agent
+{
+    private Transform tr;
+    private Rigidbody rb;
+
+    public float moveSpeed = 1.0f;
+    public float turnSpeed = 200.0f;
+    public StageManagerIM stageManager;
+
+    public override void Initialize()
+    {
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+
+}
