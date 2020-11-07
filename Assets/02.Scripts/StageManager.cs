4159850dd7702a97558400f70f8cc6938173f6c8
Lee JaeHyun <myleje@gmail.com>
2020-11-07 10:11:22 +0900

    Project Upload

---
+++ Assets/02.Scripts/StageManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class StageManager : MonoBehaviour
+{
+    public GameObject goodItem;
+    public GameObject badItem;
+
+    public int goodCount = 30;
+    public int badCount  = 30;
+
+    public List<GameObject> goodList = new List<GameObject>();
+    public List<GameObject> badList  = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitStage();
+    }
+
+    public void InitStage()
+    {
+        //GoodItem 생성
+        for (int i=0; i<goodCount; i++)
+        {
+            Vector3 pos = new Vector3( Random.Range(-24.0f, 24.0f)
+                                     , 0.05f
+                                     , Random.Range(-24.0f, 24.0f) );
+            Quaternion rot = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            goodList.Add(Instantiate<GameObject>(goodItem, pos, rot, this.transform));
+        }
+
+        //BadItem 생성
+        for (int i=0; i<badCount; i++)
+        {
+            Vector3 pos = new Vector3( Random.Range(-24.0f, 24.0f)
+                                     , 0.05f
+                                     , Random.Range(-24.0f, 24.0f) );
+            Quaternion rot = Quaternion.Euler(0, Random.Range(0, 360), 0);
+            badList.Add(Instantiate<GameObject>(badItem, pos, rot, this.transform));
+        }        
+    }
+}
